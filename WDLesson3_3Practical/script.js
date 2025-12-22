8642b81c8cb9f776ec73cc39cd926abc7398374c
chrisjsstainedurshi <christianm688@nycstudents.net>
2025-12-22 21:27:50 +0000

    update

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,12 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+document.querySelectorAll('.gallery img').forEach((image) => {
+  image.addEventListener('click', () => {
+    document.getElementById('highlight').innerText = image.alt;
+ });
+});
+
+const images = document.querySelectorAll('.gallery img');
+
+function handleMouseOver(image) {
+    image.style.border = "3px solid blue"
+
