ac42dfaed239505237cb88d2dabf9ec78c862e8e
chrisjsstainedurshi <christianm688@nycstudents.net>
2026-04-27 16:33:22 +0000

    update

--- WDLesson6_4Practical/script.js
+++ WDLesson6_4Practical/script.js
@@ -1,32 +1,26 @@
+let data;
 async function init(){
-  let link = "https://raw.githubusercontent.com/rcastro2/WebDevelopment/refs/heads/main/data/fbi.json";
-
-  let info = await fetch(link);
-  let data = await info.json();
+  
+  let link = "collision.json"
+  info = await fetch(link);
+  data = await info.json();
   
   let output = document.getElementById("output");
   let build = "";
 
-  // Traverse the data
-  data.items.forEach(criminal => {
+  for(let i = 0; i < data.length; i+=1){
+    let country = data[i];
+    build += `<div class="card">
+                <h2>${country.name}</h2>
+                <img class="fit" src="${country.flag_url}">
+                <p>Language: ${country.primary_language}</p>
+                <p>Population: ${country.population}</p>
+                <p>Continent: ${country.continent}</p>
+                <a href="${country.googleMaps}" target="_blank"> Map </a>
+              </div>`
+  }
 
-    let name = criminal.title;
-    let img = criminal.images[0]?.original || "";
-    let desc = criminal.description || "No description available";
-    let url = criminal.files[0]?.url || "#";
-
-    build += `
-      <div class="card">
-        <h3>${name}</h3>
-        <img src="${img}" class="fit">
-        <p>${desc}</p>
-        <a class="button" target="_blank" 
-        href="https://mozilla.github.io/pdf.js/web/viewer.html?file=${url}">
-        View Poster
-        </a>
-      </div>
-    `;
-  });
 
   output.innerHTML = build;
+
 }
